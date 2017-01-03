e03c9532a5421f2e6ddc89f06e334ac474d5cfce
bqasim381 <bqasim381@gmail.com>
2017-01-03 16:13:41 +0000

    added callbacks

--- js/components/board.js
+++ js/components/board.js
@@ -2,14 +2,22 @@ const React = require('react');
 const List = require('./list');
 
 class Board extends React.Component {
+    onAddInputChanged() {
+        console.log('onAddInputChanged');
+    }
+    
+    onAddSubmit(event) {
+        event.preventDefault();
+        console.log('onAddSubmit');
+    }
     
     render() {
         return(
             <div>
             {this.props.title}
-            <List title={this.props.lists[0]} cards={[1,2,3]} />
-            <List title={this.props.lists[1]} cards={[4,5,6]} />
-            <List title={this.props.lists[2]} cards={[7,8,9]} />
+            <List onChange={this.onAddInputChanged.bind(this)} onSubmit={this.onAddSubmit.bind(this)} title={this.props.lists[0]} cards={[1,2,3]} />
+            <List onChange={this.onAddInputChanged.bind(this)} onSubmit={this.onAddSubmit.bind(this)} title={this.props.lists[1]} cards={[4,5,6]} />
+            <List onChange={this.onAddInputChanged.bind(this)} onSubmit={this.onAddSubmit.bind(this)} title={this.props.lists[2]} cards={[7,8,9]} />
             </div>
             )
     }
--- js/components/list.js
+++ js/components/list.js
@@ -5,11 +5,19 @@ class List extends React.Component {
     
     render() {
         return(
+            <div className='list'>
             <div>
             {this.props.title}
             <Card text={this.props.cards[0]}/>
             <Card text={this.props.cards[1]}/>
             <Card text={this.props.cards[2]}/>
+            </div>
+            <div>
+            <form onSubmit={this.props.onSubmit}>
+            <input onChange={this.props.onChange}></input>
+            <button>Submit</button>
+            </form>
+            </div>
             </div>
             )
     }
